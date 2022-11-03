8702de9e223b1e8526a2b3f74b842604c0c52896
JerryQ <1397200108@qq.com>
2022-11-03 21:34:24 +0800

    adjust the round of random

--- trick-card.c
+++ trick-card.c
@@ -2,11 +2,13 @@
 
 void trick (int CardPileInTrick [],int len) {
   srand((unsigned int)time(0));
-  for (int i = 0;i < len;i++) {
-	int x = rand() % len;
-	int y = rand() % len;
-	int temp = CardPileInTrick[x];
-	CardPileInTrick[x] = CardPileInTrick [y];
-	CardPileInTrick[y] = temp;
+  for (int step = 0; step < 1000; ++step) {
+    for (int i = 0;i < len;i++) {
+      int x = rand() % len;
+      int y = rand() % len;
+      int temp = CardPileInTrick[x];
+      CardPileInTrick[x] = CardPileInTrick [y];
+      CardPileInTrick[y] = temp;
+    }
   }
 }
