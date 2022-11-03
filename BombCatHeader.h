704f57f47b094dce12764070da020ed7f55e1da2
Loyalty <1658364908@qq.com>
2022-11-03 15:03:14 +0800

    First, I renamed the titles of many .c files. Second, I created two .c files. One named "FunctionForSDL" contains all the SDL functions and I have moved some functions in main.c to it. The other named "Initialization" is used for initialize all the variables . by Loyalty 2022/11/03

--- BombCatHeader.h
+++ BombCatHeader.h
@@ -8,4 +8,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define WindowWidth 1280
+#define WindowHeight 720
+
+extern SDL_Window *Window; //窗口
+extern SDL_Renderer *Renderer; //画笔（渲染）
+
+extern SDL_Surface *MainBackGroundSurface; //表面它是图形
+extern SDL_Texture *MainBackGroundTexture; //轨迹，让画笔能把它画出来
+extern SDL_Rect MainBackGroundRect; //Texture的信息，长，宽，高等
+
+extern SDL_Surface *PlayBackGroundSurface;
+extern SDL_Texture *PlayBackGroundTexture;
+extern SDL_Rect PlayBackGroundRect;
+
+void LoadPicture (); //用来加载图片的
+void Begin (); //开始游戏的函数
+void QUIT (); //退出函数
+void Draw(SDL_Texture *Texture); //绘制图片
+
 #endif
